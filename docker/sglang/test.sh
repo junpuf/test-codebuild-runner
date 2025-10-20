dd8c39f0f6eb6306baa9a8b47ccdead8a18b6247
Junpu Fan <junpu@amazon.com>
2025-10-20 18:01:18 +0000

    update script

--- docker/sglang/test.sh
+++ docker/sglang/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -ex
+set -e
 CONTAINER_NAME=sglang_test
 HUGGING_FACE_HUB_TOKEN=$(aws secretsmanager get-secret-value --secret-id HUGGING_FACE_HUB_TOKEN --query SecretString --output text)
 
+set -x
 docker run --name ${CONTAINER_NAME} \
     -d --gpus=all --entrypoint /bin/bash \
     -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
@@ -12,12 +13,12 @@ docker run --name ${CONTAINER_NAME} \
     -c "python -m sglang.launch_server --model-path Qwen/Qwen3-0.6B --reasoning-parser qwen3"
 
 sleep 60
-docker logs ${CONTAINER_NAME}
+# docker logs ${CONTAINER_NAME}
 docker exec ${CONTAINER_NAME} python3 -m sglang.bench_serving \
     --backend sglang \
   	--host 127.0.0.1 --port 30000 \
   	--num-prompts 1000 \
   	--model Qwen/Qwen3-0.6B
-docker logs ${CONTAINER_NAME}
+# docker logs ${CONTAINER_NAME}
 docker stop ${CONTAINER_NAME}
 docker rm -f ${CONTAINER_NAME}
