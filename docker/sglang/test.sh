5a8f86c9d817887df8ed7f55fbe8e79581168d12
Junpu Fan <junpu@amazon.com>
2025-10-20 17:40:09 +0000

    update

---
+++ docker/sglang/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+CONTAINER_NAME=sglang_test
+HUGGING_FACE_HUB_TOKEN=$(aws secretsmanager get-secret-value --secret-id HUGGING_FACE_HUB_TOKEN --query SecretString --output text)
+docker run -d --gpus=all --entrypoint /bin/bash \
+    --name ${CONTAINER_NAME}
+    -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
+    -e "HUGGING_FACE_HUB_TOKEN=${HUGGING_FACE_HUB_TOKEN}" \
+    152553844057.dkr.ecr.us-west-2.amazonaws.com/sglang:latest \
+    -c "python -m sglang.launch_server --model-path Qwen/Qwen3-0.6B --reasoning-parser qwen3"
+
+sleep 60
+
+docker stop ${CONTAINER_NAME}
+docker rm ${CONTAINER_NAME}
