5fd7c6d3a9448b26cae1f22c15c9f9a065680233
Junpu Fan <junpu@amazon.com>
2025-10-20 18:17:33 +0000

    update

--- docker/sglang/bench_serving_qwen3_0.6b.sh
+++ docker/sglang/bench_serving_qwen3_0.6b.sh
@@ -7,16 +7,20 @@ HUGGING_FACE_HUB_TOKEN=$(aws secretsmanager get-secret-value --secret-id HUGGING
 docker run --name ${CONTAINER_NAME} \
     -d --gpus=all --entrypoint /bin/bash \
     -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
+    -v ${HOME}/dataset:/dataset \
+    -p 30000:30000 \
     -e "HUGGING_FACE_HUB_TOKEN=${HUGGING_FACE_HUB_TOKEN}" \
     152553844057.dkr.ecr.us-west-2.amazonaws.com/sglang:latest \
     -c "python -m sglang.launch_server --model-path Qwen/Qwen3-0.6B --reasoning-parser qwen3"
-
 sleep 60
 set -x
+docker logs ${CONTAINER_NAME}
 docker exec ${CONTAINER_NAME} python3 -m sglang.bench_serving \
     --backend sglang \
   	--host 127.0.0.1 --port 30000 \
   	--num-prompts 1000 \
-  	--model Qwen/Qwen3-0.6B
+  	--model Qwen/Qwen3-0.6B \
+    --dataset-name sharegpt \
+    --dataset-path /dataset/ShareGPT_V3_unfiltered_cleaned_split.json
 docker stop ${CONTAINER_NAME}
 docker rm -f ${CONTAINER_NAME}
